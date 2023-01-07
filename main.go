ae0dff2eeec7c86c10cf225fb8068f103abe1c78
Vitaliy Prushinskiy <vitprush@gmail.com>
2023-01-08 02:09:00 +0300

    update

--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 func main() {
 
 	fmt.Print("Введите ваше имя(отчество, фамилию по желанию): ")
-    scanner := bufio.NewScanner(os.Stdin)
-    scanner.Scan()
-    name := scanner.Text() // запишем вcё введеное с пробелами
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	name := scanner.Text() // запишем вcё введеное с пробелами
 
-	fmt.Print("Hello "+name+"!")
+	fmt.Print("Hello " + name + "!")
 
 }
