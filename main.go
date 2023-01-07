fa56e7fd33e312dba950a0529a8987a4423b8e9b
Vitaliy <91429783+VSP7@users.noreply.github.com>
2023-01-07 20:40:22 +0300

    Update main.go

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 
-	fmt.Print("Введите ваше имя(отчество, фамилию по желанию): ")
+    fmt.Print("Введите ваше имя(отчество, фамилию по желанию): ")
     scanner := bufio.NewScanner(os.Stdin)
     scanner.Scan()
     name := scanner.Text() // запишем вcё введеное с пробелами
 
-	fmt.Print("Hello "+name+"!")
+    fmt.Print("Hello "+name+"!")
 
 }
 
